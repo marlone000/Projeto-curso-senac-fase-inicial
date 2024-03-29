cad57eb8992675d57dca8fbff03dd59d73890b11
marlone000 <142364606+marlone000@users.noreply.github.com>
2024-03-29 20:39:47 -0300

    modificaçoes do login e cadastro

---
+++ Nova pasta/app.js
@@ -0,0 +1,73 @@
+
+// Parte da image 
+
+const slider = 
+   document.querySelector('.image-slider');
+const arrLeft = 
+   document.querySelector('.seta-esquerda');
+const arrRight =
+   document.querySelector('.seta-direita');
+const heading = 
+   document.querySelector('.rubrica h1');
+const description = 
+   document.querySelector('.rubrica p');
+   
+   //    variação image 
+
+const images = ["./IMG/berserk.png", "./IMG/luffy.png", "./IMG/boku-no-hero.png"];
+   
+const headings = [
+  
+]; 
+
+const descriptions = [
+     
+]; 
+
+// id do slider 
+
+let id = 0; 
+
+// função do slider 
+
+function slide(id) {
+    slider.style.backgroundImage = `url(${images[id]})`;
+    slider.classList.add('image-fade');
+    setTimeout(() => {
+        slider.classList.remove('image-fade');
+    }, 550);
+    heading.innerText = headings[id];
+    description.innerText = descriptions[id];
+}
+// adiciona um evento de clique a seta para esquerda
+arrLeft.addEventListener('click', () => {
+
+    // diminue id da image 
+    id--; 
+    /* verifica se o id é menor que o numero de 
+    slides disponiveis */
+    if(id < 0) {
+        // altera a ultima imagem
+        id = images.length - 1; 
+    }
+    // executa a função deslizante
+    slide(id);
+});
+
+// adiciona um evento de clicar na seta da direita 
+arrRight.addEventListener('click', () => {
+
+    // incrementar image ao id 
+    id++;
+
+    //verifique se o id é maior que o numero disponivel no slide
+    
+    if (id > images.length - 1) {
+        // muda para primeira image 
+
+        id = 0;
+    } 
+    // execute a função do slide 
+    slide(id);
+});
+
